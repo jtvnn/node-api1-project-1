2054edeab67af193d87eb60ab407e278b26f9d0f
Jason Vann <vannjt1@icloud.com>
2021-11-30 10:55:31 -0500

    post complete

--- api/server.js
+++ api/server.js
@@ -36,5 +36,29 @@ server.get('/api/users/:id', (req, res) => {
     })
 })
 
+server.put('/api/users/:id', async (req, res) => {
+    try {
+        const possibleUser = await User.findById(req.params.id)
+        if (!possibleUser) {
+            res.status(404).json({
+                message: 'The user with the specified ID does not exist'
+            })
+        } else {
+            if(!req.body.name || !req.body.bio) {
+                res.status(400).json({
+                    message: "Please provide name and bio for the user" 
+                })
+            } else {
+            const updatedUser = await User.update(req.params.id, req.body)
+            res.status(200).json(updatedUser)
+            }
+        }
+    } catch (err) {
+    res.status(500).json({
+        message: 'The user information could not be modified',
+        err: err.message
+        })
+    }
+})
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
