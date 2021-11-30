964c00b8ab4ae9b6bde0c619d2b8f533794f50f1
Jason Vann <vannjt1@icloud.com>
2021-11-30 10:12:00 -0500

    get all complete

--- api/server.js
+++ api/server.js
@@ -1,3 +1,24 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const User = require('./users/model');
+const server = express();
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.use(express.json());
+
+server.get('/api/users', (req, res) => {
+    User.find()
+    .then(users => {
+        res.json(users);
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'error getting users',
+            err: err.message
+        })
+    })
+})
+
+
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
