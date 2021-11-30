afd4556cde4849b2a3ebbbbd936e3f904c65d90d
Jason Vann <vannjt1@icloud.com>
2021-11-30 10:31:09 -0500

    get by id complete

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 // BUILD YOUR SERVER HERE
-const express = require('express');
-const User = require('./users/model');
+const express = require('express')
+const User = require('./users/model')
 const server = express();
 
 server.use(express.json());
@@ -18,7 +18,23 @@ server.get('/api/users', (req, res) => {
     })
 })
 
-
+server.get('/api/users/:id', (req, res) => {
+    User.findById(req.params.id)
+    .then(user => {
+        if (!user) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist"
+            })
+        }        
+        res.json(user)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'error getting user',
+            err: err.message
+        })
+    })
+})
 
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const server = require('./api/server');
+const server = require('./api/server')
 
-const port = 9000;
+const port = 9000
 
 // START YOUR SERVER HERE
 server.listen(port, () => {
